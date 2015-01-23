1c578b4cf9140dcdacad726482b1de7ed7bb945c
Scott Chen <rightson@gmail.com>
2015-01-23 15:26:27 +0800

    update test script

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ rootdir=examples
 uriroot=/assets
 
 # usage:
-# ./localify-cli.js css_input 							css_output							assets_location	uri_root 		[prefix]
-./localify-cli.js 	$rootdir/semantic.css 				$rootdir/semantic-offline.min.css 	$rootdir 		$uriroot 		fonts.googleapis.com.;
-./localify-cli.js 	$rootdir/fonts.googleapis.com.css 	$rootdir/fonts.googleapis.com.css   $rootdir/fonts 	$uriroot/fonts;
+# ./localify-cli.js css_input                         css_output                        assets_location  asset_uri_root [asset_prefix]
+./localify-cli.js   $rootdir/semantic.css             $rootdir/semantic-offline.min.css $rootdir         $uriroot       fonts.googleapis.com.;
+./localify-cli.js   $rootdir/fonts.googleapis.com.css $rootdir/fonts.googleapis.com.css $rootdir/fonts   $uriroot/fonts;
